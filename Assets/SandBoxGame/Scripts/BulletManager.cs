668978dcba4f393415113c6932c1254d3a081b6b
sindg0527 <sindg0527140@gmail.com>
2024-09-10 17:48:25 +0900

    이동 및 총알 발사 구현

---
+++ Assets/SandBoxGame/Scripts/BulletManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    float BulletSpeed = 10.0f;
+    Rigidbody2D rigid;
+    PlayerController playerController;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        playerController = FindObjectOfType<PlayerController>();
+
+        if (playerController.dir == 0)
+        {
+            rigid.AddForce(new Vector2(BulletSpeed, 0), ForceMode2D.Impulse);
+        }
+        else if (playerController.dir == 1)
+        {
+            rigid.AddForce(new Vector2(-BulletSpeed, 0), ForceMode2D.Impulse);
+        }
+        else if (playerController.dir == 2)
+        {
+            rigid.AddForce(new Vector2(0, -BulletSpeed), ForceMode2D.Impulse);
+            transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (playerController.dir == 3)
+        {
+            rigid.AddForce(new Vector2(0, BulletSpeed), ForceMode2D.Impulse);
+            transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+
+        //Invoke(DestroyObj(), 1000);
+    }
+
+    void DestroyObj()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/SandBoxGame/Scripts/PlayerController.cs
+++ Assets/SandBoxGame/Scripts/PlayerController.cs
@@ -1,39 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     float speed = 5.0f;
-    void Start()
+    Vector3 move;
+    public GameObject bulletObj;
+    public GameObject weaponObj;
+    public Transform bulletPos;
+    public int dir;
+    SpriteRenderer playerImage;
+    public Sprite[] rotationImage;
+
+    private void Start()
     {
-        
+        dir = 0;
+        playerImage = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        Move();
+        transform.Translate(new Vector3(move.x, move.y, 0) * speed * Time.deltaTime);
+        Rotation();
     }
 
-    private void Move()
+    void OnMovement(InputValue value)
     {
-        if (Input.GetKey(KeyCode.D))
+        move = value.Get<Vector2>();
+    }
+
+    void Rotation()
+    {
+        if (move.x > 0)
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-            transform.localEulerAngles = new Vector3(0, 0, 0);
+            dir = 0;
+            PlayerImage(dir);
+            weaponObj.transform.localEulerAngles = new Vector3(0, 0, 0);
+            //transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (move.x < 0)
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-            transform.localEulerAngles = new Vector3(0, 180, 0);
+            dir = 1;
+            PlayerImage(dir);
+            weaponObj.transform.localEulerAngles = new Vector3(0, 180, 0);
+            //weaponObj.transform.position = new Vector3(-0.187f, -0.137f, 0);
+            //transform.localEulerAngles = new Vector3(0, 180, 0);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (move.y > 0)
         {
-            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            dir = 2;
+            PlayerImage(dir);
+            weaponObj.transform.localEulerAngles = new Vector3(0, 0, 270);
+            //transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (move.y < 0)
         {
-            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            dir = 3;
+            PlayerImage(dir);
+            weaponObj.transform.localEulerAngles = new Vector3(0, 0, 90);
+            //transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    void OnFire()
+    {
+        Instantiate(bulletObj, bulletPos.transform.position, Quaternion.identity);
+    }
+
+    void PlayerImage(int count)
+    {
+        if (count == 0)
+        {
+            playerImage.sprite = rotationImage[0];
+        }
+        if (count == 1)
+        {
+            playerImage.sprite = rotationImage[1];
+        }
+        if (count == 2)
+        {
+            playerImage.sprite = rotationImage[2];
+        }
+        if (count == 3)
+        {
+            playerImage.sprite = rotationImage[3];
         }
     }
 }
